38d594666a69ee78f49c2cd1ca34cbbaf1925b0f
khappucino <khappucino@The-King-In-Yellow.local>
2017-07-16 21:27:29 -0700

    finished loginview tests

--- 2Farm2Furious/LoginViewSpec.swift
+++ 2Farm2Furious/LoginViewSpec.swift
@@ -31,7 +31,7 @@ class LoginViewSpec: KIFSpec {
             }
             
             describe("username text field") {
-                context("when we enter text into the username textfield ") {
+                describe("when we enter text into the username textfield ") {
                     beforeEach {
                         outputEvents = [LoginViewEvent]()
                         viewTester().usingIdentifier(LoginView.usernameTextfieldID).enterText("xyz")
@@ -46,8 +46,8 @@ class LoginViewSpec: KIFSpec {
                     }
                 }
                 
-                context("when we leave the textfield") {
-                    context("when the textfield still has stuff in it") {
+                describe("when we leave the textfield") {
+                    context("when the textfield still has text in it") {
                         beforeEach {
                             outputEvents = [LoginViewEvent]()
                             viewTester().usingIdentifier(LoginView.usernameTextfieldID).enterText("x")
@@ -59,7 +59,6 @@ class LoginViewSpec: KIFSpec {
                                                                     .textChanged(.username("x")), // fires on exit
                                                                     .textChanged(.username("x"))] // fires on exit
                             let isValid = expectedEvents == outputEvents
-                            print(outputEvents)
                             expect(isValid).to(beTrue())
                         }
                     }
@@ -76,6 +75,76 @@ class LoginViewSpec: KIFSpec {
                             let isValid = expectedEvents == outputEvents
                             expect(isValid).to(beTrue())
                         }
+                    }
+                }
+            }
+            describe("password text field") {
+                describe("when we enter text into the username textfield ") {
+                    beforeEach {
+                        outputEvents = [LoginViewEvent]()
+                        viewTester().usingIdentifier(LoginView.passwordTextfieldID).enterText("xyz")
+                    }
+                    
+                    it("should emit a LoginViewEvent") {
+                        let expectedEvents: [LoginViewEvent] = [.textChanged(.password("x")),
+                                                                .textChanged(.password("xy")),
+                                                                .textChanged(.password("xyz"))]
+                        let isValid = expectedEvents == outputEvents
+                        expect(isValid).to(beTrue())
+                    }
+                }
+                
+                describe("when we leave the textfield") {
+                    context("when the textfield still has text in it") {
+                        beforeEach {
+                            outputEvents = [LoginViewEvent]()
+                            viewTester().usingIdentifier(LoginView.passwordTextfieldID).enterText("x")
+                            subject.passwordTextField.resignFirstResponder()
+                        }
+                        
+                        it("should emit a LoginViewEvent") {
+                            let expectedEvents: [LoginViewEvent] = [.textChanged(.password("x")), // entering text
+                                                                    .textChanged(.password("x")), // fires on exit
+                                                                    .textChanged(.password("x"))] // fires on exit
+                            let isValid = expectedEvents == outputEvents
+                            print(outputEvents)
+                            expect(isValid).to(beTrue())
+                        }
+                    }
+                    
+                    context("when the textfield is empty when we end editing") {
+                        beforeEach {
+                            outputEvents = [LoginViewEvent]()
+                            viewTester().usingIdentifier(LoginView.passwordTextfieldID).enterText("")
+                            subject.passwordTextField.resignFirstResponder()
+                        }
+                        
+                        it("should emit a LoginViewEvent") {
+                            let expectedEvents: [LoginViewEvent] = [.textChanged(.password(""))] // fires on exit
+                            let isValid = expectedEvents == outputEvents
+                            expect(isValid).to(beTrue())
+                        }
+                    }
+                }
+            }
+            
+            describe("the submit button") {
+                describe("when we tap the submit button") {
+                    beforeEach {
+                        
+                        // no matter what is in the textfields we just propagate the values
+                        viewTester().usingIdentifier(LoginView.usernameTextfieldID).enterText("whatever value")
+                        viewTester().usingIdentifier(LoginView.passwordTextfieldID).enterText("")
+                        
+                        outputEvents = [LoginViewEvent]()
+                        
+                        viewTester().usingIdentifier(LoginView.submitButtonID).tap()
+                    }
+                    
+                    it("should emit a LoginViewEvent") {
+                        let expectedEvents: [LoginViewEvent] = [.submitButtonPressed(.username("whatever value"), .password(""))]
+                        let isValid = expectedEvents == outputEvents
+                        expect(isValid).to(beTrue())
                     }
                 }
             }
